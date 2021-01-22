287174b4cf4c41cbe190f85eb9f215e134ed8863
IrinaX <iksanova08@mail.ru>
2021-01-22 23:51:56 +0500

    todo:sort by surname

---
+++ main.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Implement a notebook that stores personal information (last name, first name, age, gender, phone number).
+ * Provide sorting of records by last name and search by age.
+ */
+
+typedef struct {
+    char name[15];
+    char surname[20];
+    int age;
+    char gender[7];
+    char telNumber[12];
+} person;
+
+person addPerson(char name[], char surname[], int age, char gender[], char telNumber[]);
+
+unsigned int getAmountOfPeople();
+
+void getPeopleData(unsigned int amountOfPeople, person *notebook);
+
+void printNotebook(unsigned int amountOfPeople, person *notebook);
+
+void printMenu();
+
+int getCorrectValue(int limit);
+
+int getMenuVariant(int count);
+
+unsigned int getNumberOfMatches(int age, person *notebook, unsigned int amountOfPeople);
+
+void findPeople(int age, person *notebook, unsigned int amountOfPeople, person *foundPeople);
+
+int getAge(int maxAge);
+
+int main() {
+    unsigned int amountOfPeople = getAmountOfPeople();
+    person notebook[amountOfPeople];
+    getPeopleData(amountOfPeople, notebook);
+    printNotebook(amountOfPeople, notebook);
+    int variant = 0;
+    while (variant != 3) {
+        printMenu();
+        variant = getMenuVariant(3);
+        switch (variant) {
+            case 1:
+                printf("sort list");
+                break;
+            case 2: {
+                int age = getAge(100);
+                unsigned int numberOfMatches = getNumberOfMatches(age, notebook, amountOfPeople);
+                if (numberOfMatches != 0) {
+                    person foundPeople[numberOfMatches]; //you need to determine the size of the array before filling it, dynamic memory allocation of 10 lab
+                    findPeople(age, notebook, amountOfPeople, foundPeople);
+                    printNotebook(numberOfMatches, foundPeople);
+                }
+                break;
+            }
+            case 3:
+                printf("exit");
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+//    system("pause");
+    return 0;
+}
+
+void getPeopleData(unsigned int amountOfPeople, person *notebook) {
+    char name[15];
+    char surname[20];
+    int age;
+    char gender[7];
+    char telNumber[12];
+    int numeration;
+    for (int i = 0; i < amountOfPeople; ++i) {
+        numeration = i + 1;
+        printf("%d person. Input name:", numeration);
+        scanf("%s", name);
+
+        printf("%d person. Input surname:", numeration);
+        scanf("%s", surname);
+
+        printf("%d person. Input age:", numeration);
+        scanf("%d", &age);
+
+        printf("%d person. Input gender:", numeration);
+        scanf("%s", gender);
+
+        printf("%d person. Input telNumber:", numeration);
+        scanf("%s", telNumber);
+        printf("\n");
+        notebook[i] = addPerson(name, surname, age, gender, telNumber);
+    }
+}
+
+person addPerson(char *name, char *surname, int age, char *gender, char *telNumber) {
+    person addedPerson;
+    strcpy(addedPerson.name, name);
+    strcpy(addedPerson.surname, surname);
+    addedPerson.age = age;
+    strcpy(addedPerson.gender, gender);
+    strcpy(addedPerson.telNumber, telNumber);
+    return addedPerson;
+}
+
+unsigned int getAmountOfPeople() {
+    unsigned int amountOfPeople;
+    printf("Input amount of people: ");
+    scanf("%d", &amountOfPeople);
+    return amountOfPeople;
+}
+
+void printNotebook(unsigned int amountOfPeople, person *notebook) {
+    int numeration;
+    for (int i = 0; i < amountOfPeople; ++i) {
+        numeration = i + 1;
+        printf("%d) name: %s \tsurname: %s \tage: %d \tgender: %s \ttelNumber: %s\n", numeration, notebook[i].name,
+               notebook[i].surname, notebook[i].age, notebook[i].gender, notebook[i].telNumber);
+    }
+}
+
+void printMenu() {
+    printf("\nWhat do you want to do?\n");
+    printf("1. Sort list\n");
+    printf("2. Searching people by age\n");
+    printf("3. Exit\n");
+    printf(">");
+}
+
+int getCorrectValue(int limit) {
+    int result;
+    char string[100];// строка для считывания введённых данных
+    scanf("%s", string); // считываем строку
+    // пока ввод некорректен, сообщаем об этом и просим повторить его
+    while (sscanf(string, "%d", &result) != 1 || result < 1 || result > limit) {
+        printf("Incorrect input. Try again: "); // выводим сообщение об ошибке
+        scanf("%s", string); // считываем строку повторно
+    }
+    return result;
+}
+
+int getMenuVariant(int count) {
+    int variant = getCorrectValue(count);
+    return variant;
+}
+
+int getAge(int maxAge) {
+    printf("Enter age: ");
+    int age = getCorrectValue(maxAge);
+    return age;
+}
+
+void findPeople(int age, person *notebook, unsigned int amountOfPeople, person *foundPeople) {
+    int j = 0;
+    for (int i = 0; i < amountOfPeople; ++i) {
+        if (notebook[i].age == age) {
+            foundPeople[j] = notebook[i];
+            ++j;
+        }
+    }
+}
+
+unsigned int getNumberOfMatches(int age, person *notebook, unsigned int amountOfPeople) {
+    int numberOfMatches = 0;
+    for (int i = 0; i < amountOfPeople; ++i) {
+        if (notebook[i].age == age) {
+            ++numberOfMatches;
+        }
+    }
+    if (numberOfMatches == 0) {
+        printf("No matches found.");
+        return 0;
+    } else {
+        return numberOfMatches;
+    }
+}
+
+
+
+
